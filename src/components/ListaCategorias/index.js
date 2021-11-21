bf2ea2496717dcca073dbcdf98964b02ea595496
Daniel Ferraz <daniel0ferraz@outlook.com>
2021-11-21 00:58:12 -0300

    fix api and category

--- src/components/ListaCategorias/index.js
+++ src/components/ListaCategorias/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css'
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export default function ListaCategorias() {
