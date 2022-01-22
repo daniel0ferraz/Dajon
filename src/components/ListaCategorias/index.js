5598ca138aca5df0bc5bb5d5d7f2d3a43a5f9747
Daniel Ferraz <Daniel0ferraz@outlook.com>
2022-01-21 21:49:33 -0300

    fix merge conflictss

--- src/components/ListaCategorias/index.js
+++ src/components/ListaCategorias/index.js
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from 'react';
-<<<<<<< HEAD
 import * as S from "./styles";
 import { Link } from 'react-router-dom';
-=======
-import './styles.css'
-import { useParams } from 'react-router-dom';
->>>>>>> 5c876023143608772a34b2516f932665ecb36f2f
 import axios from 'axios';
 
 export default function ListaCategorias() {
   const [categorias, setCategorias] = useState([]);
-
 
   useEffect(() => {
     axios.get(`http://localhost:5000/categorias/`)
